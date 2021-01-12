4097ce16a9897da2104e0c84f6919ed8b0e0b82f
Harut Margaryan <harut.msu@gmail.com>
2021-01-12 14:28:50 +0400

    uploaded assets

---
+++ Assets/OldPathfinding.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[ExecuteInEditMode]
+public class OldPathfinding : MonoBehaviour {
+	public Camera cam;
+	public UnityEngine.AI.NavMeshAgent agent;
+	public GameObject door;
+	public GameObject navMesh;
+	public GameObject[] frontPoints;
+
+	// Use this for initialization
+	void Start () {
+		cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+		door = GameObject.Find("Door");
+		if (!(IsIgnorableByNavMesh(door))) {
+			MakeObjectIgnorableByNavMesh(door);
+		}
+
+		navMesh = CreateNavMesh();
+		NavMeshSurface navMeshSurface = navMesh.GetComponent<NavMeshSurface>();
+		navMeshSurface.BuildNavMesh();
+
+		// GameObject newPlayer = CreatePlayer(door.transform.position);
+		GameObject debugStartPoint = GameObject.Find("DebugStartPoint");
+		// GameObject newPlayer = CreatePlayer(debugStartPoint.transform.position);
+
+		// agent = newPlayer.GetComponent<NavMeshAgent>();
+		frontPoints = GameObject.FindGameObjectsWithTag("frontPoint");
+		NavMeshPath path = new NavMeshPath();
+		foreach (GameObject frontPoint in frontPoints) {
+			if (!(IsIgnorableByNavMesh(frontPoint))) {
+				MakeObjectIgnorableByNavMesh(frontPoint);
+			}
+			// var watch = System.Diagnostics.Stopwatch.StartNew();
+	        // agent.CalculatePath(frontPoint.transform.position, path);
+	        NavMesh.CalculatePath(
+	        	door.transform.position,
+	        	// debugStartPoint.transform.position,
+	        	frontPoint.transform.position,
+	        	NavMesh.AllAreas,
+	        	path
+	        );
+			// watch.Stop();
+			// var elapsedMs = watch.ElapsedMilliseconds;
+			Debug.Log(frontPoint.name + ": " + path.status);
+		}	
+	}
+	
+	// bool isPositionReachable(position) {
+
+	// }
+
+	static GameObject CreateNavMesh() {
+		GameObject surface = new GameObject();
+		surface.name = "NavMeshGenerated";
+		surface.transform.position = new Vector3(0, 0, 0);
+		surface.AddComponent<NavMeshSurface>();
+		return surface;
+	}
+
+	static GameObject CreatePlayer(Vector3 position) {
+		GameObject newPlayer = new GameObject();
+		newPlayer.name = "Player";
+		//newPlayer.transform.position = new Vector3(-9.3f, 1.25f, -4.94f);
+		newPlayer.transform.position = position;
+		newPlayer.AddComponent<NavMeshAgent>();
+		return newPlayer;
+	}
+
+	static void MakeObjectIgnorableByNavMesh(GameObject obj) {
+		obj.AddComponent<NavMeshModifier>();
+		obj.GetComponent<NavMeshModifier>().ignoreFromBuild = true;
+	}
+
+	static bool IsIgnorableByNavMesh(GameObject obj) {
+		NavMeshModifier navMeshModifier = obj.GetComponent<NavMeshModifier>();
+		if (navMeshModifier == null) {
+			return false;
+		}
+		return navMeshModifier.ignoreFromBuild;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+	public Camera cam;
+	public NavMeshAgent agent;
+	public GameObject door;
+
+	void Start () {
+		GameObject newPlayer = CreatePlayer();
+		agent = newPlayer.GetComponent<NavMeshAgent>();
+
+		Debug.Log(agent.transform.position);
+		door = GameObject.Find("Door");
+		Debug.Log(door.transform.position);
+		// agent.nextPosition(door.transform.position);
+		agent.transform.position = door.transform.position;
+		Debug.Log(agent.transform.position);
+	}	
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				// MOVE OUR AGENT
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+
+	GameObject CreatePlayer() {
+		GameObject newPlayer = new GameObject();
+		newPlayer.name = "Player2";
+		newPlayer.transform.position = new Vector3(-9.3f, 1.25f, -4.94f);
+		newPlayer.AddComponent<NavMeshAgent>();
+		return newPlayer;
+	}
+
+	// void Update() {
+	// 	if (Input.GetMouseButtonDown(0))
+	// 	{
+	// 		Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+	// 		RaycastHit hit;
+
+	// 		if (Physics.Raycast(ray, out hit))
+	// 		{
+	// 			NavMeshPath path = new NavMeshPath();
+	// 			var watch = System.Diagnostics.Stopwatch.StartNew();
+	// 	        agent.CalculatePath(hit.point, path);
+	// 			watch.Stop();
+	// 			var elapsedMs = watch.ElapsedMilliseconds;
+	// 			Debug.Log(elapsedMs);
+	// 			Debug.Log(path.status);
+	// 	        // if (path.status == NavMeshPathStatus.PathPartial)
+	// 	        // {
+	// 	        // 	Debug.Log("True");
+	// 	        // }
+	// 	        // else 
+	// 	        // {
+	// 	        // 	Debug.Log("False");
+	// 	        // }
+	// 		}
+	// 	}
+	// }
+}
